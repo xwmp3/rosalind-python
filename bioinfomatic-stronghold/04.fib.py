be1b380b24833c5789430183bddc98c19eef7b3c
Miner Wang <minerwang@nwafu.edu.cn>
2022-07-01 16:07:52 +0800

    Add dataset

--- bioinfomatic-stronghold/04.fib.py
+++ bioinfomatic-stronghold/04.fib.py
@@ -1,5 +1,12 @@
 # https://rosalind.info/problems/fib/
 
+def load_nk(filepath: str):
+    n, k = 0, 0
+    with open(filepath, 'r') as f:
+        n, k = [int(d) for d in f.readline().replace('\n', '').strip().split()]
+    print(f'n: {n}, k: {k}')
+    return n, k
+
 def rabbit_fib(n: int, k: int):
     if n == 1 or n == 2:
         return 1
@@ -7,6 +14,6 @@ def rabbit_fib(n: int, k: int):
         return rabbit_fib(n - 1, k) + k * rabbit_fib(n - 2, k)
     
 if __name__ == "__main__":
-    n = 5
-    k = 3
+    path = "./datasets/04.fib.txt"
+    n, k = load_nk(path)
     print(rabbit_fib(n, k))
---
+++ bioinfomatic-stronghold/07.iprb.py
@@ -0,0 +1,37 @@
+# https://rosalind.info/problems/iprb/
+
+import numpy as np
+
+def load_kmn(filepath: str):
+    k, m, n = 0, 0, 0
+    with open(filepath, 'r') as f:
+        k, m, n = [int(d) for d in f.readline().replace('\n', '').strip().split()]
+    print(f'k: {k}, m: {m}, n: {n}')
+    return k, m, n
+
+def iprb(k: int, m: int, n: int):
+    num_all = k + m + n
+    # 后代为显性个体
+    pr_list = []
+    # AA x any
+    pr_list.append(k / num_all)
+    # Aa x AA
+    pr_list.append(m / num_all * k / (num_all - 1))
+    # Aa x Aa
+    pr_list.append(m / num_all * (m - 1) / (num_all - 1) * 3 / 4)
+    # Aa x aa
+    pr_list.append(m / num_all * n / (num_all - 1) * 1 / 2)
+    # aa x AA
+    pr_list.append(n / num_all * k / (num_all - 1))
+    # aa x Aa
+    pr_list.append(n / num_all * m / (num_all - 1) * 1 / 2)
+    # aa x aa
+    pr_list.append(0)
+    
+    return np.sum(pr_list)
+    
+    
+if __name__ == "__main__":
+    path = './datasets/07.iprb.txt'
+    k, m, n = load_kmn(path)
+    print(iprb(k, m, n))
