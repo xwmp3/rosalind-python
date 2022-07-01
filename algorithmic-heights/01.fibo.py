8706f21fe34a9051c61b16c257df5403ebb8b2c9
Miner Wang <minerwang@nwafu.edu.cn>
2022-07-01 17:22:21 +0800

    Add folders

---
+++ algorithmic-heights/01.fibo.py
@@ -0,0 +1,20 @@
+# https://rosalind.info/problems/fibo/
+
+def fib(n: int):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+def load_n(filepath: str):
+    n = 0
+    with open(filepath, 'r') as f:
+        n = int(f.readline().replace('\n', '').strip())
+    return n
+    
+if __name__ == "__main__":
+    path = "./datasets/01.fibo.txt"
+    n = load_n(path)
+    print(fib(n))
---
+++ bioinfomatics-stronghold/08.prot.py
@@ -0,0 +1,31 @@
+def load_rna_codon_table(filepath: str):
+    rna_prot_map = {}
+    with open(filepath, 'r') as f:
+        for line in f.readlines():
+            datas = line.replace('\n', '').strip().split()
+            for i in range(4):
+                rna_seq, prot = datas[2 * i], datas[2 * i + 1]
+                rna_prot_map[rna_seq] = prot
+    return rna_prot_map
+
+def load_rna_seq(filepath: str):
+    seq = ""
+    with open(filepath, 'r') as f:
+        seq = f.readline().replace('\n', '').strip()
+    print(f'rna_seq: {seq}')
+    return seq
+    
+def rna2prot(rna_prot_map: dict, seq: str):
+    prot_list = []
+    for i in range(int(len(seq) / 3)):
+        prot = rna_prot_map[seq[3 * i: 3 * i + 3]]
+        if prot == 'Stop': break
+        prot_list.append(prot)
+    return ''.join(prot_list)
+    
+if __name__ == "__main__":
+    codon_table_path = "./datasets/08.rna-codon-table.txt"
+    data_path = "./datasets/08.prot.txt"
+    rna_prot_map = load_rna_codon_table(codon_table_path)
+    rna_seq = load_rna_seq(data_path)
+    print(rna2prot(rna_prot_map, rna_seq))
