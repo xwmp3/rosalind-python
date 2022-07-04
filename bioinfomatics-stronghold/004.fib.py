a709d2b491b09645b0e9d35bf640bc40e2d9b8f9
Miner Wang <minerwang@nwafu.edu.cn>
2022-07-04 21:06:48 +0800

    Update problem 'Rabbits and Recurrence Relations' and 'Mortal Fibonacci Rabbits' with both recursive and non-recursive solution

--- bioinfomatics-stronghold/004.fib.py
+++ bioinfomatics-stronghold/004.fib.py
@@ -7,13 +7,23 @@ def load_nk(filepath: str):
     print(f'n: {n}, k: {k}')
     return n, k
 
-def rabbit_fib(n: int, k: int):
+def rabbit_fib_recursive(n: int, k: int):
     if n == 1 or n == 2:
         return 1
     else:
-        return rabbit_fib(n - 1, k) + k * rabbit_fib(n - 2, k)
+        return rabbit_fib_recursive(n - 1, k) + k * rabbit_fib_recursive(n - 2, k)
+    
+def rabbit_fib_non_recursive(n: int, k: int):
+    fib_table = []
+    for i in range(n):
+        if i < 2:
+            fib_table.append(1)
+        else:
+            fib_table.append(fib_table[-1] + k * fib_table[-2])
+    return fib_table[n - 1]
     
 if __name__ == "__main__":
     path = "./datasets/004.fib.txt"
     n, k = load_nk(path)
-    print(rabbit_fib(n, k))
+    print(f"recursive:\t{rabbit_fib_recursive(n, k)}")
+    print(f"non-recursive:\t{rabbit_fib_non_recursive(n, k)}")
--- bioinfomatics-stronghold/011.fibd.py
+++ bioinfomatics-stronghold/011.fibd.py
@@ -1,20 +1,27 @@
 # https://rosalind.info/problems/fibd/
 
-import numpy as np
+def fibd_recursive(n: int, m: int):
+    if n == 1 or n == 2:
+        return 1
+    elif n <= m:
+        return fibd_recursive(n - 1, m) + fibd_recursive(n - 2, m)
+    elif n == m + 1:
+        return fibd_recursive(n - 1, m) + fibd_recursive(n - 2, m) - fibd_recursive(n - m, m)
+    else:
+        return fibd_recursive(n - 1, m) + fibd_recursive(n - 2, m) - fibd_recursive(n - m - 1, m)
 
-def fibd_none_recursive(n: int, m: int):
+def fibd_non_recursive(n: int, m: int):
     fib_table = []
     for i in range(n):
         if i < 2:
             fib_table.append(1)
         elif i < m:
             fib_table.append(fib_table[-2] + fib_table[-1])
-        elif i == m or i == m + 1:
+        elif i == m:
             fib_table.append(fib_table[-2] + fib_table[-1] - fib_table[-m])
         else:
             fib_table.append(fib_table[-2] + fib_table[-1] - fib_table[-(m + 1)])
     return fib_table[n - 1]
-
     
 if __name__ == '__main__':
     path = './datasets/011.fibd.txt'
@@ -22,4 +29,5 @@ if __name__ == '__main__':
     with open(path, 'r') as f:
         n, m = [int(d) for d in f.readline().replace('\n', '').strip().split()]
     print(f"n: {n}, m: {m}")
-    print(fibd_none_recursive(n, m))
+    # print(f"recursive: \t{fibd_recursive(n, m)}")
+    print(f"non-recursive: \t{fibd_non_recursive(n, m)}")
