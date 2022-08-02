5efcf0de95e66caab0e41f8eaccca869af588aac
Miner Wang <minerw99@gmail.com>
2022-08-02 11:26:03 +0800

    Create 'utils.py' to put common functions together

---
+++ algorithmic-heights/utils.py
@@ -0,0 +1,8 @@
+def load_edge_List(filepath: str) -> (int, int, list):
+    n, m, edges = 0, 0, []
+    with open(filepath, 'r') as f:
+        n, m = [int(item) for item in f.readline().replace('\n', '').strip().split()]
+        for _ in range(m):
+            start_node, end_node = [int(item) for item in f.readline().replace('\n', '').strip().split()]
+            edges.append((start_node, end_node))
+    return n, m, edges
