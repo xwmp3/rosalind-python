bb7d597c4e8c42cb75166661e9f4bb0c0ce29c10
Miner Wang <minerw99@gmail.com>
2022-09-29 22:35:11 +0800

    update: Add a function implementing the Permutation itself.

--- bioinfomatics-stronghold/019.perm.py
+++ bioinfomatics-stronghold/019.perm.py
@@ -1,26 +1,53 @@
 # https://rosalind.info/problems/perm/
 
-from itertools import permutations
+import itertools
+
+from utils import list_2_str
 
 
 def load_n(filepath: str) -> int:
-    n: int = 0
     with open(filepath, 'r') as f:
         n = int(f.readline().replace('\n', '').strip())
     return n
 
 
+# https://docs.python.org/zh-cn/3/library/itertools.html#itertools.permutations
+def permutations(iterable, r=None):
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    if r > n:
+        return
+    indices = list(range(n))
+    cycles = list(range(n, n - r, -1))
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i + 1:] + indices[i:i + 1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
+        else:
+            return
+
+
 def get_permutation_list(n: int):
     n_list = [i for i in range(1, n + 1)]
-    print(n_list)
-    return [iter_list for iter_list in permutations(n_list)]
+    res = []
+    for permutation in permutations(n_list):
+        res.append(permutation)
+    return res
 
 
 if __name__ == '__main__':
-    path = "datasets/019.perm.in"
-    n = load_n(path)
-    print(n)
-    permutate_list = get_permutation_list(n)
+    inpath = "datasets/019.perm.in"
+
+    permutate_list = get_permutation_list(load_n(inpath))
     print(len(permutate_list))
-    for iter_list in permutate_list:
-        print(' '.join([str(item) for item in iter_list]))
+    for perm in permutate_list:
+        print(list_2_str(perm))
